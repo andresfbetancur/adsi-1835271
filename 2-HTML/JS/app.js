5ece621d5f18dce47d4251ee31866b72176c1dfb
andresfbetancur <andresf0betancurm@gmail.com>
2019-05-20 17:23:23 -0500

    imagenes y listas

---
+++ 2-HTML/JS/app.js
@@ -0,0 +1,9 @@
+
+var ans = document.getElementById('answer');
+var qns = prompt ('Cual es su nombre?');
+
+if (qns == 'ofac'){
+	ans.innerHTML = "Bienvenido Administrador"
+}else{
+	ans.innerHTML = "Bienvenido loca"
+}
